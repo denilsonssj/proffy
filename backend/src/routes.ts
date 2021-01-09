52bc9d38d56741892d45468d4e68bb44419ba15b
Name <email@email.com>
2021-01-08 21:44:06 -0300

    Add controllers to Class and Connection and updating the routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,5 +1,14 @@
 import { Router } from 'express';
 
+import ClassController from './controllers/ClassController';
+import ConnectionController from  './controllers/ConnectionController';
+
 const routes = Router();
+
+routes.post('/classes', ClassController.create);
+routes.get('/classes', ClassController.index);
+
+routes.post('/connections', ConnectionController.create);
+routes.get('/connections', ConnectionController.index);
 
 export default routes;
