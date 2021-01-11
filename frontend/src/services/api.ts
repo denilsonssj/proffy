fdd188ac891d07f76da4e2e04bd34e5dd2014a32
Name <email@email.com>
2021-01-11 16:34:12 -0300

    Add api service

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const HOST = process.env.REACT_APP_BACKEND_HOST || 'http://127.0.0.1';
+const PORT = process.env.REACT_APP_BACKEND_PORT || '3000';
+
+const api = axios.create({
+    baseURL: `${HOST}/${PORT}` || 'http://127.0.0.1/3333',
+});
+
+export default api;
