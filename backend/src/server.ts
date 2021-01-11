916d31864fca816e7e9ca6d5a90e17f6f8f3155e
Name <email@email.com>
2021-01-11 16:45:25 -0300

    Edit server.ts fix cors configuration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,8 +6,8 @@ import routes from './routes';
 const PORT = process.env.PORT || 3333;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use(cors);
 
 app.listen(PORT);
