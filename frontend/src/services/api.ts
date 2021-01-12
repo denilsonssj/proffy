e8c49362ef0efd2c5476ea6e8bf3db8e14471926
Name <email@email.com>
2021-01-12 14:23:22 -0300

    Fix backend url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -4,7 +4,7 @@ const HOST = process.env.REACT_APP_BACKEND_HOST || 'http://127.0.0.1';
 const PORT = process.env.REACT_APP_BACKEND_PORT || '3000';
 
 const api = axios.create({
-    baseURL: `${HOST}/${PORT}` || 'http://127.0.0.1/3333',
+    baseURL: `${HOST}:${PORT}/` || 'http://127.0.0.1:3333/',
 });
 
 export default api;
